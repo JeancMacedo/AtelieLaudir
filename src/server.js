9c8201be7a5e1e159b36d838c78a002665a3458b
Joao Gimenez <jvg020804@gmail.com>
2025-10-14 21:32:02 -0300

    teste primeira config server.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,25 @@
-//a.
+const express = require('express');
+const app = express();
+
+
+const porta = 3000;
+
+
+app.set("view engine" ,"ejs");
+
+app.set("views","./View");
+
+
+
+app.get("/",(req,res)=>{
+
+    res.render("test");
+
+})
+
+app.listen(3000,() => {
+    console.log(`Servidor rodando na porta ${porta}`);    
+})
+
+
+
